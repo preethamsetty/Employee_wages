17ee9ea90e6980c7e63767ee34dc6134b0cfb6bc
preethamsetty <preethambs2002@gmail.com>
2024-09-23 11:33:41 +0530

    [Preetham B S] add - modified a file

--- UC3/uc3.js
+++ UC3/uc3.js
@@ -1,4 +1,4 @@
-// UC3: Add Part-time Employee & Wage
+// UC3: Add Part-time Employee & Wage using if-else if
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
 const FULL_TIME_HOURS = 8;
@@ -8,20 +8,18 @@ const WAGE_PER_HOUR = 20;
 function calculateEmployeeWage() {
     let empHours = 0;
     let empCheck = Math.floor(Math.random() * 3); // 0, 1, or 2 (Absent, Full-time, or Part-time)
-    switch (empCheck) {
-        case IS_FULL_TIME:
-            empHours = FULL_TIME_HOURS;
-            console.log("Employee is Full-time");
-            break;
-        case IS_PART_TIME:
-            empHours = PART_TIME_HOURS;
-            console.log("Employee is Part-time");
-            break;
-        default:
-            empHours = 0;
-            console.log("Employee is Absent");
-            break;
+    
+    if (empCheck === IS_FULL_TIME) {
+        empHours = FULL_TIME_HOURS;
+        console.log("Employee is Full-time");
+    } else if (empCheck === IS_PART_TIME) {
+        empHours = PART_TIME_HOURS;
+        console.log("Employee is Part-time");
+    } else {
+        empHours = 0;
+        console.log("Employee is Absent");
     }
+
     let dailyWage = empHours * WAGE_PER_HOUR;
     console.log("Daily Wage: $" + dailyWage);
 }
